8001a77fb16273c797c8ce1dc139366037ae4e27
ruwandigeekiyanage <ruwandi.2001.18@gmail.com>
2021-10-06 16:43:17 +0530

    something

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const {MongoClient} = require('mongodb');
 require('dotenv').config()
 const mongoose = require('mongoose');
-const PORT = process.env.SERVER_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || localhost;
 
 const app = express()
