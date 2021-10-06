56bee51cea95037c7c9f5593165b2b60bd8f5364
ruwandigeekiyanage <ruwandi.2001.18@gmail.com>
2021-10-06 10:33:48 +0530

    env

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express")
+require('dotenv').config()
+const mongoose = require('mongoose');
+
+const PORT = process.env.SERVER_PORT;
+
 const app = express()
 
 app.get("/", function(req, res){
     res.send("working well though")
 })
 
-app.listen(process.env.PORT || 5000)
+mongoose.connect(
+    'mongodb://127.0.0.1:27017/todos',
+).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Todos Service Up and Running on ${PORT}`);
+    })
+}).catch((error => {
+    console.log(error)
+}));
